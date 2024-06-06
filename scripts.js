a32d503d2356247a11eefd12db6c80fe4672a543
wambura <meshack.sirincha@student.moringaschool.com>
2024-06-07 08:04:37 +0900

    add expert in landing page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,1 @@
-// select modal btn, modal overlay, close btn
-// const modalBtn = document.querySelector(".show-overlay");
-// const modal = document.querySelector(".modal-overlay");
-// const closeBtn = document.querySelector(".close-btn");
 
-// listen for click events on modalBtn and Close btn
-// when user clicks modal btn , add open modal to modal overlay
-// when user clicks close modal, remove open modal from modal overlay
-
-// modalBtn.addEventListener("click", function () {
-//   modal.style.display = "block";
-// });
-// closeBtn.addEventListener("click", function () {
-//   modal.style.display = "none";
-// });
